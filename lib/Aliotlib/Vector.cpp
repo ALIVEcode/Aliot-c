56833d75c1bc5b1862fda469a1172777acdfee16
Mathis Laroche <ecomath360@gmail.com>
2022-04-01 13:46:32 -0400

    Added comments to Vector

--- lib/Aliotlib/Vector.cpp
+++ lib/Aliotlib/Vector.cpp
@@ -43,17 +43,15 @@ public:
     {
         if (this->size == this->capacity)
         {
-            this->capacity *= 2;
-            T *newData = new T[this->capacity];
-            for (int i = 0; i < this->size; i++)
-            {
+            this->capacity *= 2;                 // double the capacity
+            T *newData = new T[this->capacity];  // allocate new memory
+            for (int i = 0; i < this->size; i++) // copy old data to new data
                 newData[i] = this->data[i];
-            }
-            delete[] this->data;
-            this->data = newData;
+            delete[] this->data;  // free old data
+            this->data = newData; // assign new data
         }
-        this->data[this->size] = element;
-        this->size++;
+        this->data[this->size] = element; // add element to vector
+        this->size++;                     // increase size
     }
 
     // remove an element from the vector
@@ -61,9 +59,9 @@ public:
     {
         for (int i = 0; i < this->size; i++)
         {
-            if (this->data[i] == element)
+            if (this->data[i] == element) //
             {
-                this->removeAt(i);
+                this->removeAt(i); // remove element at index i
                 break;
             }
         }
@@ -72,25 +70,22 @@ public:
     // remove an element from the vector at a given index
     void removeAt(int index)
     {
-        if (index < 0 || index >= this->size)
-        {
+        if (index < 0 || index >= this->size) // check if index is valid
             return;
-        }
-        for (int i = index; i < this->size - 1; i++)
-        {
+
+        for (int i = index; i < this->size - 1; i++) // shift elements to the left
             this->data[i] = this->data[i + 1];
-        }
-        this->size--;
+
+        this->size--; // decrease size
     }
 
     // get the element at a given index
     T get(int index)
     {
-        if (index < 0 || index >= this->size)
-        {
+        if (index < 0 || index >= this->size) // check if index is valid
             return NULL;
-        }
-        return this->data[index];
+
+        return this->data[index]; // return element at index
     }
 
     // get the size of the vector
@@ -108,7 +103,7 @@ public:
     // remove all elements from the vector
     void clear()
     {
-        this->size = 0;
+        this->size = 0; // reset size
     }
 
     // get the index of the first occurence of an element
@@ -116,11 +111,9 @@ public:
     {
         for (int i = 0; i < this->size; i++)
         {
-            if (this->data[i] == element)
-            {
-                return i;
-            }
+            if (this->data[i] == element) //  if element is found
+                return i;                 // return index
         }
-        return -1;
+        return -1; // element not found
     }
 };
